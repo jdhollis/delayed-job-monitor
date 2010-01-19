c4f4aeb80d09d828b23c790f69cc849a925e75d1
J.D. Hollis <jd@densityofspace.com>
2010-01-19 13:42:21 -0500

    Added alerts for failed jobs.

--- delayed_job_monitor.rb
+++ delayed_job_monitor.rb
@@ -9,11 +9,23 @@ class DelayedJobMonitor < Scout::Plugin
   def build_report
     establish_database_connection
     
+    failed = DelayedJob.all(:conditions => 'failed_at IS NOT NULL')
+    unless failed.blank?
+      previously_failed = memory(:failed) || []
+      newly_failed = failed.reject { |job| previously_failed.include? job.id }
+      
+      newly_failed.each { |job|
+        alert "Failed job #{ job.id }", "#{ job.handler }\n#{ job.last_error }"
+      }
+      
+      remember :failed, failed.map { |j| j.id }
+    end
+    
     report :total => DelayedJob.count
     report :running => DelayedJob.count(:conditions => 'locked_at IS NOT NULL')
     report :scheduled => DelayedJob.count(:conditions => ['run_at > ? AND locked_at IS NULL AND attempts = 0', Time.now.utc])
     report :failing => DelayedJob.count(:conditions => 'attempts > 0 AND failed_at IS NULL AND locked_at IS NULL')
-    report :failed => DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
+    report :failed => failed.size
   end
 
 private
