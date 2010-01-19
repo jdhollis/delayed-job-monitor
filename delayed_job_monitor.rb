2b2b4dd89bfdf0759e8e0718350e2a68c115192f
J.D. Hollis <jd@densityofspace.com>
2010-01-19 12:16:18 -0500

    A couple of tweaks.

--- delayed_job_monitor.rb
+++ delayed_job_monitor.rb
@@ -1,21 +1,19 @@
 class DelayedJobMonitor < Scout::Plugin
-  needs 'activerecord', 'yaml'
+  needs 'active_record', 'yaml'
   
-  require 'activerecord'
+  require 'active_record'
   
-  module Delayed
-    class Job < ActiveRecord::Base
-    end
+  class DelayedJob < ActiveRecord::Base
   end
   
   def build_report
     establish_database_connection
     
-    report :total => Delayed::Job.count
-    report :running => Delayed::Job.count(:conditions => 'locked_at IS NOT NULL')
-    report :scheduled => Delayed::Job.count(:conditions => ['run_at > ? AND locked_at IS NULL AND attempts = 0', Time.now.utc])
-    report :failing => Delayed::Job.count(:conditions => 'attempts > 0 AND failed_at IS NULL AND locked_at IS NULL')
-    report :failed => Delayed::Job.count(:conditions => 'failed_at IS NOT NULL')
+    report :total => DelayedJob.count
+    report :running => DelayedJob.count(:conditions => 'locked_at IS NOT NULL')
+    report :scheduled => DelayedJob.count(:conditions => ['run_at > ? AND locked_at IS NULL AND attempts = 0', Time.now.utc])
+    report :failing => DelayedJob.count(:conditions => 'attempts > 0 AND failed_at IS NULL AND locked_at IS NULL')
+    report :failed => DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
   end
 
 private
